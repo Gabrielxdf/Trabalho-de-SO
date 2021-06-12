e098e1944ca0af0792849c0686907aa0a04924f1
Gabriel Henrique <gabrielxdf16@gmail.com>
2021-06-12 20:40:07 -0300

    Iniciando o trabalho com um template de código em C

---
+++ produtor_consumidor.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!\n");
+   system("pause");
+   return 0;
+}
