1e2663f97843c067b37d3d225786adc04da9f882
Gabriel Henrique <gabrielxdf16@gmail.com>
2021-06-18 23:09:24 -0300

    sistema de tempo implementado e corrigido o erro do pthread_join()

--- soma_de_vetores.c
+++ soma_de_vetores.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
-#include <semaphore.h>
 #include <locale.h>
 #include <time.h>
 
 //declaração das variáveis globais
+unsigned int contador_thread = 0;
 float *x;
 float *y;
 float *z;
@@ -34,12 +34,19 @@ void *preencheVetores(void *argPtr){
     unsigned int inicio = thread_p->posInicial;
     unsigned int final = thread_p->posFinal;
 
+    printf("A thread %u processará os elementos dos vetores nas posições de %u a %u \n", contador_thread,
+     thread_p->posInicial, thread_p->posFinal);
+    contador_thread++;
+
     for(;inicio <= final; inicio++){
         pthread_mutex_lock(&mutexes[thread_p->contMutexThread]);
-        thread_p->vetor[inicio] = (float)rand()/(float)(RAND_MAX/1.0);
-        printf("x[%u] e o valor é: %f \n", inicio, x[inicio]);
+        thread_p->vetor[inicio] = (float)rand()/(float)(RAND_MAX/1.0); 
+        //printf("x[%u] e o valor é: %f \n", inicio, x[inicio]);   
+        //y[inicio] = (float)rand()/(float)(RAND_MAX/1.0); //resolver depois.
+        //z[inicio] = x[inicio] + y[inicio];
         pthread_mutex_unlock(&mutexes[thread_p->contMutexThread]);
     }
+    pthread_mutex_destroy(&mutexes[thread_p->contMutexThread]);
     pthread_exit(0);
 }
 
@@ -66,28 +73,37 @@ int main() {
     y =(float*) malloc(sizeof(float)*tamanho_vetor);
     z =(float*) malloc(sizeof(float)*tamanho_vetor);
     mutexes = (pthread_mutex_t*) malloc(sizeof(pthread_mutex_t)*n);
+
     int divisao_vetor[n+1];
     for (int i = 0; i <= n; i++){
         divisao_vetor[i] = ((tamanho_vetor/n)*(i));
     }
+
+    clock_t inicio = clock();
+    //inicialização das threads e mutexes.
     pthread_t thread[n];
+    Thread_preenche thread_preenche[n];
     for (int i = 0; i < n; ++i){
-    pthread_mutex_init(&mutexes[i],NULL);
-    Thread_preenche thread_preenche = {
-        .vetor = &x[0],
-        .posInicial = (divisao_vetor[i]),
-        .posFinal = divisao_vetor[i+1]-1,
-        .contMutexThread = i
-    };
-       pthread_create(&thread[i], NULL, preencheVetores, &(thread_preenche));
-       //jeito errado, o join logo em seguida do create da thread.
-       //pthread_join(thread[i], NULL);
+        thread_preenche[i].vetor = &x[0];
+        thread_preenche[i].posInicial = (divisao_vetor[i]);
+        thread_preenche[i].posFinal = divisao_vetor[i+1]-1;
+        thread_preenche[i].contMutexThread = i;
+
+        pthread_mutex_init(&mutexes[i],NULL);
+        pthread_create(&thread[i], NULL, preencheVetores, &(thread_preenche[i]));
     }
-    //Assim seria o jeito certo de fazer, o join sendo depois
-    //mas não está funcionando.
+
     for (int i = 0; i < n; i++){
         pthread_join(thread[i], NULL);
     }
+    clock_t fim = clock();
+    double tempo_gasto = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    
+    //impressão dos resultados.
+    for (int i = 0; i < tamanho_vetor; i++){
+    //printf("[%u]    x = %f    y = %f    z = %f \n", i, x[i], y[i], z[i]);
+    }
+    printf("Tempo de execução: %lf segundo(s). \n", tempo_gasto);
 
     free(x);
     free(y);
@@ -95,7 +111,7 @@ int main() {
     free(mutexes);
 
     #ifdef _WIN32
-        system("pause");
+        //system("pause");
     #else __linux__
         system("read -p 'Press Enter to continue...\n' key");
     #endif
